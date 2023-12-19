90428a9777f9bf07de816f14daeb83d980498c46
RavoPL <ravosilesia88@protonmail.com>
2023-12-19 21:30:19 +0000

    add initial wishlist functionality python foundations

---
+++ wishlist/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
+
+# Create your views here.
+
+
+def view_wishlist(request, user_id):
+    """ A view that renders the wishlist contents page """
+
+    return render(request, 'wishlist/wishlist.html')
+
+# Adding items to the wishlist
+
+def add_to_wishlist(request, item_id, user_id):
+    """ Add a quantity of the specified product to the wishlist """
+
+    product = get_object_or_404(Product, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+
+    return redirect(redirect_url)
+
+
+# Removing items from the wishlist
+
+def remove_from_wishlist(request, item_id, user_id):
+    """ Remove the item from the wishlist """
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+        else:
+            bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
