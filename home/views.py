abf085d9b85705051690222b9a6f90d1f5971851
RavoPL <ravosilesia88@protonmail.com>
2023-12-19 16:50:10 +0000

    add contact page link

--- home/views.py
+++ home/views.py
@@ -7,3 +7,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def contact(request):
+    """ A view to return the contact page """
+
+    return render(request, 'contact/contact.html')
