98da607aaab9b30701a3f4abb67a2fc6c646af83
RavoPL <ravosilesia88@protonmail.com>
2023-12-20 19:47:11 +0000

    add Wishlist frontend buttons for widescreen and mobile view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,50 +1,35 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
-from products.models import Product
+from products.models import Product, Wishlist
 
 # Create your views here.
 
-
+# Viewing the wishlist
+@login_required
 def view_wishlist(request, user_id):
     """ A view that renders the wishlist contents page """
 
     return render(request, 'wishlist/wishlist.html')
 
 # Adding items to the wishlist
-
+@login_required
 def add_to_wishlist(request, item_id, user_id):
     """ Add a quantity of the specified product to the wishlist """
 
     product = get_object_or_404(Product, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
 
+    messages.info(request,'The item was added to your wishlist.')
+
     return redirect(redirect_url)
 
 
 # Removing items from the wishlist
-
+@login_required
 def remove_from_wishlist(request, item_id, user_id):
     """ Remove the item from the wishlist """
 
-    try:
-        product = get_object_or_404(Product, pk=item_id)
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        bag = request.session.get('bag', {})
+    product = get_object_or_404(Product, pk=item_id)
 
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
-        else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
-
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+    return redirect(redirect_url)
